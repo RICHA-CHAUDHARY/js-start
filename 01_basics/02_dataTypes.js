9d6b6688f3fec02f82abf0d5add1f7c32e368317
RICHA CHAUDHARY <richa.chaudhary197@gmail.com>
2025-07-12 14:19:48 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS Code as newer version
+
+//alert( 3 + 3) //we are using nodejs, not browser
+
+console.log(3 
+    + 3) // code readility should be high
+
+console.log("Richa")
+
+
+let name = "richa"
+let age = 21;
+let isLoggedIn = false
+let state;
+
+// number = 2 to power 53
+// bigint
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined = 
+// symbol = unique
+
+//object
+
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
+
