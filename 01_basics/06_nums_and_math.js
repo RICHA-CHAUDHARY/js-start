fe3d8150d9aca8a0ad0743ec1fcff9e13614f799
RICHA CHAUDHARY <richa.chaudhary197@gmail.com>
2025-07-12 21:43:22 +0530

    Math and Num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,45 @@
+const score = 400
+// console.log(score);
+
+
+const balance = new Number(100)
+//console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.8966 // roundoff
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000 // add comma system in number
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//**********************************8 MATHS **************************8***********8 */
+
+// console.log(Math);
+// console.log(Math.abs(-4));//negative change positive
+// console.log(Math.round(4.6));//round off
+// console.log(Math.ceil(4.2));// if small change in 4 then they give 5
+// console.log(Math.floor(4.2));// if small change in 4 they give only 4 value
+// console.log(Math.min(4, 3, 6, 8));
+
+//where to use
+console.log(Math.random()); // always give value to 0 to 1
+console.log((Math.random()*10) + 1); //other than 0 or 1
+console.log(Math.floor(Math.random()*10) + 1); // roundoff value give 1 to 9
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min); //avoid 0 and value comes btw 10 and 20
+
+
+
+
+
+
+
+
+
