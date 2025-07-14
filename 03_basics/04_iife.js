4ee8119e6b9fb492b2e88ffff8037940231e482b
RICHA CHAUDHARY <richa.chaudhary197@gmail.com>
2025-07-14 14:53:37 +0530

    IIFA

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,15 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+(function chai(){
+    // named IIFe
+    console.log(`DB CONNECTED`);
+})(); // always end the code with ; sign then the other code will run
+
+((name) => {
+    // unnamed IIFe
+    console.log(`DB CONNECTED ${name}`);
+    
+} ) ('richa')
+   
+
+// pollution of global scope is create problem, so we have to remove that pollution use iife.
