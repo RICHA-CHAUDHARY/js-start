21b7722bbe6402d00b45ff87e4592c5819333b54
RICHA CHAUDHARY <richa.chaudhary197@gmail.com>
2025-07-15 11:31:12 +0530

    loops

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,40 +1,17 @@
-// for
+let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
 
-for (let index = 0; index < 10; index++) {
-    const element = index;
-    if (element == 5) {
-       // console.log("5 is best number");
-    }
-    //console.log(element);
+let myArray = ["flash","batman","superman"]
+let arr = 0
+while (arr < myArray.length) {
+   // console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
 }
-//console.log(element);
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
 
-for (let index = 1; index <= 10; index++) {
-   // console.log(`Outer loop value: ${index}`);
-    
-    for (let j = 1; j <= 10; j++) {
-       // console.log(`Inner loop value: ${j} and inner loop ${index}`);
-       //console.log(index + '*' + j + ' = ' + index*j );
-    }
-}
-let myArray = ["flash","batman","super"]
-console.log(myArray.length);
-
-for (let index = 0; index < myArray.length; index++) {
-    const element = myArray[index];
-    console.log(element);
-}
-
-
-// break and continue
-
-for (let index = 1; index <= 20; index++) {
-    if (index == 5) {
-        console.log(`Detected 5`);
-        //break                  // control flow break;
-        continue                 // condition one time skip 
-    }
-    console.log(`Value of i is ${index}`);
-    
-    
-}
---
+++ 05_iterations/two.js
@@ -0,0 +1,40 @@
+// for
+
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    if (element == 5) {
+       // console.log("5 is best number");
+    }
+    //console.log(element);
+}
+//console.log(element);
+
+for (let index = 1; index <= 10; index++) {
+   // console.log(`Outer loop value: ${index}`);
+    
+    for (let j = 1; j <= 10; j++) {
+       // console.log(`Inner loop value: ${j} and inner loop ${index}`);
+       console.log(index + '*' + j + ' = ' + index*j );
+    }
+}
+let myArray = ["flash","batman","super"]
+console.log(myArray.length);
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+   // console.log(element);
+}
+
+
+// break and continue
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+      //  console.log(`Detected 5`);
+        //break                  // control flow break;
+        continue                 // condition one time skip 
+    }
+   // console.log(`Value of i is ${index}`);
+    
+    
+}
