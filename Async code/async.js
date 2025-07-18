2cb444c7f6b9f9b2be3efbc35e1edfd8d64f870b
RICHA CHAUDHARY <richa.chaudhary197@gmail.com>
2025-07-18 14:43:33 +0530

    projects 5 6

---
+++ Async code/async.js
@@ -0,0 +1,14 @@
+// javascript = it is synchronous(line by line code execute) // it is single threaded // it is default
+
+// execution context - execute one line of code at a time. // each operation waits for last one to complete before executing.
+
+// 1. console log-1
+// 2. console log-2  // call stack and Memory heap
+
+// Blocking Code - block the flow of diagram/ read file Sync
+// Non blocking code - does not block execution / read file Async
+
+
+
+
+
